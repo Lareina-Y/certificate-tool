84752284054e276713b80c7ab7c1bef81916c653
”Shiwen <sh836690@dal.ca>
2021-05-11 23:27:38 -0300

    count required and elective courses number

--- js/button.js
+++ js/button.js
@@ -3,21 +3,31 @@ function clickOn(el) {
     var total = document.getElementById("total");
     var value = total.innerHTML;
 
+    //number add 1 for the required/elective courses check
+    var check = el.className + "i";
+    var number = document.getElementsByClassName(check);
+
+
     if(x[0].style.opacity != 1){
         for (i = 0; i < x.length; i++) {
             x[i].style.opacity = 1;
         }
-
+        for (i = 0; i < number.length; i++) {
+            number[i].innerHTML ++;
+        }
         value++;
     } else {
         for (i = 0; i < x.length; i++) {
             x[i].style.opacity = 0.3;
         }
-        
+        for (i = 0; i < number.length; i++) {
+            number[i].innerHTML --;
+        }
         value--;
     }
 
     document.getElementById("total").innerHTML = value;
+    document.getElementsByClassName(check) = number;
 }
 
 function resetClick() {
